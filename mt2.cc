aab5e487e0fda102b9f7b9c8d111e8c74d0345ac
cbpark <cbpark@gmail.com>
2021-03-04 00:28:51 +0900

    Minor modification

--- mt2.cc
+++ mt2.cc
@@ -8,13 +8,18 @@ int main() {
     double m_chi = 100.0;
 
     asymm_mt2_lester_bisect::disableCopyrightMessage();
-    // Note that the argumemts of the get_mT2 function are:
-    //   asymm_mt2_lester_bisect::get_mT2(
-    //       m_vis1, px_vis1, py_vis1, m_vis2,
-    //       px_vis2, py_vis2, px_miss, py_miss,
-    //       m_invis1, m_invis2);
+    /*
+     * Note that the argumemts of the get_mT2 function are:
+     *   asymm_mt2_lester_bisect::get_mT2(
+     *       m_vis1, px_vis1, py_vis1,
+     *       m_vis2, px_vis2, py_vis2,
+     *       px_miss, py_miss,
+     *       m_invis1, m_invis2);
+     */
     double mt2 = asymm_mt2_lester_bisect::get_mT2(
-        vis_a[0], vis_a[1], vis_a[2], vis_b[0], vis_b[1], vis_b[2], ptmiss[0],
-        ptmiss[1], m_chi, m_chi);
+        vis_a[0], vis_a[1], vis_a[2],
+        vis_b[0], vis_b[1], vis_b[2],
+        ptmiss[0], ptmiss[1],
+        m_chi, m_chi);
     std::cout << "MT2: " << mt2 << '\n';
 }
